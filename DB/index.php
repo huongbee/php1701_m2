87a4ca656e891e516a657dc4bc818ba9069c1f20
Huong <admin@Apples-MacBook-Pro.local>
2018-04-13 18:39:17 +0700

    DBConnect

--- DB/index.php
+++ DB/index.php
@@ -4,4 +4,22 @@ require 'connect.php';
 
 $db = new DBConnect;
 
-$query = "DELETE FROM users WHERE id=33";
+//$query = "DELETE FROM users WHERE id=33";
+$query = "INSERT INTO users(
+        `username`,
+        `password`,
+        fullname,
+        birthdate,
+        gender,
+        address,
+        email,
+        phone
+    ) 
+    VALUES(?,?,?,?,?,?,?,?)";
+$params = ['admin02','13212121','Huong','1996-04-13','nữ', "LTR Q1",'admin02@gmail.com','23456788765'];
+
+$result = $db->executeQuery($query);
+// if($result) echo 'success';
+// else echo 'error';
+
+echo $result ? 'success':"error";
